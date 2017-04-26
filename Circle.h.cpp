67a0069d73b88fc3685a2e613f2c97a87d6c3026
metamorphos <1143246240@qq.com>
2017-04-27 00:32:19 +0800

    Circle.h

---
+++ Circle.h.cpp
@@ -0,0 +1,14 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+class Circle
+{
+public:
+      void Circle(double R);//构造函数
+      void Area();//求面积函数
+private:
+      double R;//
+      double S;
+}；
+
+#endif//防止重复编译
