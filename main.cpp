4d3c885b828e634daad006f0a6d125ed01757e34
metamorphos <1143246240@qq.com>
2017-04-27 15:23:36 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"Circle.h"
+using namespace std;
+
+int main()
+{
+     Circle Circle();
+     Circle.Circle(double R);//调用Circle函数
+     Circle.Area();//调用Area函数
+     return 0;
+}
